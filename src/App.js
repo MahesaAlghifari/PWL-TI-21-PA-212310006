d5b6bd70df63db43ecb56cef9a1cda3eeac64ae6
MahesaAlghifari <212310006@student.ibik.ac.id>
2024-03-20 21:37:27 +0700

    lab pertemuan 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import BaseRoute from "./apps/BaseRoute";
+import LayoutInit from "./components/LayoutInit";
 
-function App() {
+// Menambahkan gaya latar belakang hitam ke elemen root
+document.body.style.backgroundColor = "#252837";
+function App({ basename }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <BrowserRouter>
+      <LayoutInit>
+        <div className="container">
+        <BaseRoute />
+        </div>
+      </LayoutInit>
+    </BrowserRouter>
+    
   );
 }
+
 
 export default App;
